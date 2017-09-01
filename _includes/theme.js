39d52f3f681e3821da2f8146cacec4531ddcc4c2
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2017-09-01 19:14:03 +0200

    Add proper theme toggle, a11y fixes

--- _includes/theme.js
+++ _includes/theme.js
@@ -3,32 +3,47 @@ function disableDarkTheme() {
     document.getElementById("dark-syntax"),
     document.getElementById("dark-style")
   ].forEach(function (a) {
-    a && document.body.removeChild(a)
+    a && document.body.removeChild(a);
   })
 }
 
 function enableDarkTheme() {
   var a = document.createElement("link");
-  a.id = "dark-style", a.rel = "stylesheet", a.href = "{{ site.url }}/assets/css/dark-style.css", a.media = "screen";
+  a.id = "dark-style";
+  a.rel = "stylesheet";
+  a.href = "{{ site.url }}/assets/css/dark-style.css";
+  a.media = "screen";
 
   var b = document.createElement("link");
-  b.id = "dark-syntax", b.rel = "stylesheet", b.href = "{{ site.url }}/assets/css/dark-syntax.css", b.media = "screen";
+  b.id = "dark-syntax";
+  b.rel = "stylesheet";
+  b.href = "{{ site.url }}/assets/css/dark-syntax.css";
+  b.media = "screen";
 
   var c = document.createDocumentFragment();
-  c.appendChild(a), c.appendChild(b), document.body.appendChild(c)
+  c.appendChild(a);
+  c.appendChild(b);
+
+  document.body.appendChild(c);
 }
 
-function toggleDarkTheme() {
-  JSON.parse(sessionStorage["dark-style"]) ? enableDarkTheme() : disableDarkTheme()
+function applyTheme(isDarkTheme) {
+  var toggleIcon = document.getElementById("theme-toggle");
+  toggleIcon.classList.toggle("icon-dark", !isDarkTheme);
+  toggleIcon.classList.toggle("icon-light", isDarkTheme);
+  isDarkTheme ? enableDarkTheme() : disableDarkTheme();
 }
 
-var avatar = document.querySelector("#theme-toggle");
-avatar.title = "Toggle Dark Theme",
-avatar.style.cursor = "pointer",
-avatar.addEventListener("click", function () {
-  var a = JSON.parse(sessionStorage["dark-style"]);
+var toggle = document.querySelector("#theme-toggle");
+toggle.title = "Toggle Dark Theme";
+toggle.style.cursor = "pointer";
+toggle.addEventListener("click", function () {
+  var applyDarkTheme = !JSON.parse(sessionStorage["dark-style"]);
+  applyTheme(applyDarkTheme);
+  sessionStorage["dark-style"] = applyDarkTheme;
+});
 
-  sessionStorage["dark-style"] = !a, toggleDarkTheme()
-}), document.addEventListener("DOMContentLoaded", function () {
-  sessionStorage["dark-style"] || (sessionStorage["dark-style"] = !1), toggleDarkTheme()
+document.addEventListener("DOMContentLoaded", function () {
+  !sessionStorage["dark-style"] && (sessionStorage["dark-style"] = false)
+  applyTheme(JSON.parse(sessionStorage["dark-style"]));
 });
